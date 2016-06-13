4769d59f04a90d504635789ffa85543d9f34a40c
Matt Latham <latham75@gmail.com>
2016-06-13 14:39:15 -0500

    preloaded and add message function

---
+++ js/addNew.js
@@ -0,0 +1,11 @@
+var Chatty = (
+	function applyText(newChat){
+		if (event.keyCode === 13){
+		var newObj = {"message": input.value};
+		messages.push(newObj);				
+	}
+
+	
+})(Chatty)
+
+
--- js/message.js
+++ js/message.js
@@ -11,11 +11,22 @@ xhr.send();
 
 function loadedFile(){
 	console.log("file loaded");
-	var firstList = JSON.parse(xhr.responseText);
-	// function to apply to DOM;
+	var preloaded = JSON.parse(xhr.responseText);
+	applyText(preloaded)
 };
 
 function failedFile(){
 	alert("file failed to load");
 };
+
+function applyText(object, array){
+	var counter = 0, messageCard;
+	for (var i = 0; i < object.length; i++) {
+		messageCard = `<div class="lightbox"><h3>${object.messages[i].message}</h3><button id="card-${counter}">Delete</button></div>`;
+		// output.innerHTML += messageCard;
+		counter++;
+	};
+};	
+
+
 })(Chatty || {});
