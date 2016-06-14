18e1780026ca4ad287ae5adafbb7559082688bfd
Matt Latham <latham75@gmail.com>
2016-06-14 11:29:54 -0500

    attempt to merge

--- js/message.js
+++ js/message.js
@@ -1,32 +1,12 @@
-var Chatty = (function getMessage(newchat){
-
-var xhr = new XMLHttpRequest();
-
-xhr.addEventListener("load", loadedFile)
-xhr.addEventListener("failed", failedFile)
-
-xhr.open("GET", "messages.json");
-
-xhr.send();
-
-function loadedFile(){
-	console.log("file loaded");
-	var preloaded = JSON.parse(xhr.responseText);
-	applyText(preloaded)
-};
-
-function failedFile(){
-	alert("file failed to load");
-};
-
-function applyText(object, array){
-	var counter = 0, messageCard;
-	for (var i = 0; i < object.length; i++) {
-		messageCard = `<div class="lightbox"><h3>${object.messages[i].message}</h3><button id="card-${counter}">Delete</button></div>`;
-		// output.innerHTML += messageCard;
+var Chatty = (function applyText(object){
+	var counter = 0;
+	for (var i = 0; i < object.messages.length; i++) {
+		var messageCard = `<div class="lightbox"><h3>${object.messages[i].message}</h3><button id="card-${counter}">Delete</button></div>`;
+		container.innerHTML += messageCard;
 		counter++;
+		console.log("apply", messageCard);
 	};
-};	
+}
+)(Chatty);	
 
 
-})(Chatty || {});
