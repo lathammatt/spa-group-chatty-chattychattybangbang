006aed85475467c88f84d931fbefcf1f10a23d4a
Alex Horton <alexhortonmusic@gmail.com>
2016-06-14 14:08:35 -0500

    complete nav bar/navbar.js

--- js/addNew.js
+++ js/addNew.js
@@ -5,13 +5,14 @@ var Chatty = (
 		test.addEventListener("keyup", function () {
 		if(event.keyCode === 13){
 		var newObj = {"message": test.value};
-
 		preloaded.messages.push(newObj);
 		var messageCard = `<div class="lightbox" id="mess-${(preloaded.messages.length)-1}"><h3>${preloaded.messages[(preloaded.messages.length)-1].message}</h3><button id="card-${preloaded.messages.length-1}">Delete</button></div>`;
 		messageWindow.innerHTML += messageCard;
 		console.log("newChat", preloaded.messages);
-		test.value = ""
-	}
+		test.value = "";
+		clearBtn.classList.remove('disableBtn');
+		};
+
 });
 	return newChat;
 	return preloaded;
