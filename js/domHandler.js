784b8e8f621a70faa61c680b35331d3fdfbe1080
Matt Latham <latham75@gmail.com>
2016-06-20 15:45:49 -0500

    altering code for jquery

--- js/domHandler.js
+++ js/domHandler.js
@@ -1,33 +1,39 @@
+$(document).ready(function () {
 var messageInput = document.getElementById('messageInput');
-var darkTheme = document.getElementById('darkTheme');
-var largeText = document.getElementById('largeText');
+// var darkTheme = $("#darkTheme");
+// var largeText = document.getElementById('largeText');
 var messageWindow = document.getElementById('messageWindow');
 
 
-darkTheme.addEventListener('click', function darkThemeEvent () {
-	messageWindow.classList.toggle('darkThemeColors');
+// darkTheme.addEventListener('click', function darkThemeEvent () {
+// 	messageWindow.classList.toggle('darkThemeColors');
+// });
+
+$("#darkTheme").on("click", function darkThemeEvent() {
+	$("#messageWindow").toggleClass("darkThemeColors")});
+
+// largeText.addEventListener('click', function largeTextEvent () {
+// 	messageWindow.classList.toggle('largeTextLetters');
+// });
+
+$("#largeText").on("click", function largeTextEvent(){
+	$("#messageWindow").toggleClass("largeTextLetters");
 });
 
-largeText.addEventListener('click', function largeTextEvent () {
-	messageWindow.classList.toggle('largeTextLetters');
-});
+var preloaded = {};
 
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("load", loadedFile)
-xhr.addEventListener("failed", failedFile)
-
-xhr.open("GET", "messages.json");
-
-xhr.send();
-
-var preloaded = "";
+$.ajax({url:"messages.json"}).done(loadedFile);
 
 
-function loadedFile(){
-	preloaded = JSON.parse(xhr.responseText);
-	console.log('HELLO', preloaded);
-	applyText(preloaded);
+
+function loadedFile(result){
+	// preloaded = JSON.parse(xhr.responseText);
+	$(result).each(function (key, value){
+		preloaded += value;
+	});
+	console.log('HELLO', typeof preloaded, preloaded);
 	return preloaded;
+	applyText(preloaded);
 };
 
 function failedFile(){
@@ -38,7 +44,8 @@ function applyText(object){
 	var counter = 0;
 	for (var i = 0; i < object.messages.length; i++) {
 		var messageCard = `<div class="lightbox" id="mess-${counter}"><h3>${object.messages[i].message}</h3><button id="card-${counter}">Delete</button></div>`;
-		messageWindow.innerHTML += messageCard;
+		$("#messageWindow").append(messageCard);
 		counter++;
 	};
 };
+});
